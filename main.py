212f001566ebd7b8c86de4d99e6333c578467d13
LapTop_Bogdan <bogdanvoinov90@gmail.com>
2023-10-26 21:14:27 +0300

    release version database

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ from personal_chat_commands import router as personal_chat_router
 from headmen_reg_commands import router as headmen_reg_router
 from headmen_commands import router as headmen_cmd_router
 from poll import router as poll_router, job
-from config.config_reader import config
 from service import UsersService
 from callbacks import router as callback_router
 
@@ -18,7 +17,7 @@ from callbacks import router as callback_router
 async def main():
     storage = MemoryStorage()  # Создаем хранилище
 
-    bot = Bot(config.BOT_TOKEN.get_secret_value())  # Получаем токен бота из файла с конфигом
+    bot = Bot('6650110444:AAFt6ac-uqfMaEZXx7IcE1myXz1kk1cC0IE')  # Получаем токен бота из файла с конфигом
     dp = Dispatcher(storage=storage)  # Создаем диспетчер и передаем ему храналище
     dp.include_routers(personal_chat_router, headmen_reg_router,
                        poll_router, callback_router, headmen_cmd_router)  # Добавляем роутеры в диспетчер
@@ -30,8 +29,8 @@ async def main():
 
     scheduler = AsyncIOScheduler(timezone="Europe/Moscow")
     scheduler.add_job(job,'cron', day_of_week='mon-sun', hour=7, minute=00, args=(1, bot.send_message))
-    scheduler.add_job(job, 'interval', seconds=60, args=(1, bot.send_message))
-    await job(1, bot.send_message)
+    # scheduler.add_job(job, 'interval', seconds=60, args=(1, bot.send_message))
+    # await job(1, bot.send_message)
     scheduler.start()
 
     logging.info('bot is starting')
