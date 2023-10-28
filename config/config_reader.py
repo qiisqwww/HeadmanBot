48862fa233589a02a4a0be5d3d1909c1387d02a1
qiisqwww <qiisqwww@gmail.com>
2023-10-28 13:33:19 +0300

    config commit

--- config/config_reader.py
+++ config/config_reader.py
@@ -4,9 +4,9 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 __all__ = ["config"]
 
 class Settings(BaseSettings):
-    BOT_TOKEN: SecretStr = "6650110444:AAFt6ac-uqfMaEZXx7IcE1myXz1kk1cC0IE"
-    PASSWORD: SecretStr = "hh;Pl8w"
-    model_config = SettingsConfigDict(env_file='config\.env.txt', env_file_encoding='utf-8')
+    BOT_TOKEN: SecretStr
+    PASSWORD: SecretStr
+    model_config = SettingsConfigDict(env_file='config/.env.txt', env_file_encoding='utf-8')
 
 
 
--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-python3 main.py
+source venv/bin/activate
+python3 main.py &
