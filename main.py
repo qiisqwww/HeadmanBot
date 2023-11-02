9100c2179f892d55d5d2f7b08f54e91e26660389
NeoTheBestDeveloper <neothebestmain@protonmail.com>
2023-11-02 17:34:57 +0300

    ci: delete .env file from git repo and add file pyproject.toml.
    'pyproject.toml' - file for python projects configuration. I've added it
    for configuration of my tools (mypy, pylint etc.).

--- main.py
+++ main.py
@@ -1,45 +1,61 @@
 import asyncio
 import logging
-import datetime
 
-from apscheduler.schedulers.asyncio import AsyncIOScheduler
-from aiogram import Dispatcher, Bot
+from aiogram import Bot, Dispatcher
 from aiogram.fsm.storage.memory import MemoryStorage
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
-from personal_chat_commands import router as personal_chat_router
-from headman_reg_commands import router as headmen_reg_router
-from headman_commands import router as headmen_cmd_router
-from poll import router as poll_router, job
-from service import UsersService
 from callbacks import router as callback_router
 from config.config_reader import config
+from headman_commands import router as headmen_cmd_router
+from headman_reg_commands import router as headmen_reg_router
+from personal_chat_commands import router as personal_chat_router
+from poll import job
+from poll import router as poll_router
+from service import UsersService
 
 
 async def main():
     storage = MemoryStorage()  # Создаем хранилище
 
-    bot = Bot(config.BOT_TOKEN.get_secret_value())  # Получаем токен бота из файла с конфигом
+    bot = Bot(
+        config.BOT_TOKEN.get_secret_value()
+    )  # Получаем токен бота из файла с конфигом
     dp = Dispatcher(storage=storage)  # Создаем диспетчер и передаем ему храналище
-    dp.include_routers(personal_chat_router, headmen_reg_router,
-                       poll_router, callback_router, headmen_cmd_router)  # Добавляем роутеры в диспетчер
+    dp.include_routers(
+        personal_chat_router,
+        headmen_reg_router,
+        poll_router,
+        callback_router,
+        headmen_cmd_router,
+    )  # Добавляем роутеры в диспетчер
 
-    logging.basicConfig(filename='logs/logs.logs', level=logging.DEBUG, format="%(asctime)s %(levelname)s %(message)s")  # Указываем файл для логирования
+    logging.basicConfig(
+        filename="logs/logs.logs",
+        level=logging.DEBUG,
+        format="%(asctime)s %(levelname)s %(message)s",
+    )  # Указываем файл для логирования
 
     with UsersService() as con:
         con.create_table()
 
     scheduler = AsyncIOScheduler(timezone="Europe/Moscow")
-    scheduler.add_job(job,'cron', day_of_week='mon-sun', hour=7, minute=00, args=(bot.send_message,))
-    #scheduler.add_job(job, 'interval', seconds=60, args=(bot.send_message, ))
-    #await job(bot.send_message)
+    scheduler.add_job(
+        job, "cron", day_of_week="mon-sun", hour=7, minute=00, args=(bot.send_message,)
+    )
+    # scheduler.add_job(job, 'interval', seconds=60, args=(bot.send_message, ))
+    # await job(bot.send_message)
     scheduler.start()
 
-    logging.info('bot is starting')
+    logging.info("bot is starting")
 
-    await bot.delete_webhook(drop_pending_updates=True)  # Игнорируем все команды, отправленные до запуска бота
+    await bot.delete_webhook(
+        drop_pending_updates=True
+    )  # Игнорируем все команды, отправленные до запуска бота
     await dp.start_polling(bot)  # Запуск бота
 
     logging.info("bot was turned off")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     asyncio.run(main())
