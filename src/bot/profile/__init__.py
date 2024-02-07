a91858e175d4f212f9526e737c3f6789da2dff68
qiisqwww <qiisqwww@gmail.com>
2024-02-07 14:26:38 +0300

    refactor: all profile routers are now 'united' into one profile router

--- src/bot/profile/__init__.py
+++ src/bot/profile/__init__.py
@@ -1,13 +1,5 @@
-from .command import include_profile_command_router
-from .callbacks import (
-    include_profile_menu_router,
-    include_ask_updated_field_validity_router
-)
-from .finite_state import include_profile_update_router
+from .profile_router import include_profile_router
 
 __all__ = [
-    "include_profile_command_router",
-    "include_profile_menu_router",
-    "include_profile_update_router",
-    "include_ask_updated_field_validity_router"
+    "include_profile_router"
 ]
