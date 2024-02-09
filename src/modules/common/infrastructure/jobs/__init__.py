8d0f51761ee8cdea458693c5c14b9f719cecdfb7
qiisqwww <qiisqwww@gmail.com>
2024-02-09 10:21:28 +0300

    feat: added inform_about_update_job to notify users about new updates

--- src/modules/common/infrastructure/jobs/__init__.py
+++ src/modules/common/infrastructure/jobs/__init__.py
@@ -1,5 +1,7 @@
 from .sending_job import SendingJob
+from .inform_about_update_job import InformAboutUpdateJob
 
 __all__ = [
     "SendingJob",
+    "InformAboutUpdateJob"
 ]
--- src/modules/common/infrastructure/jobs/inform_about_update_job.py
+++ src/modules/common/infrastructure/jobs/inform_about_update_job.py
@@ -7,13 +7,12 @@ from aiogram import Bot
 from aiogram.exceptions import TelegramForbiddenError
 from injector import Injector
 
-from src.bot.poll_attendance.resources import POLL_TEMPLATE, update_attendance_buttons
-from src.bot.poll_attendance.resources.templates import student_was_not_polled_warning_template
-from src.modules.attendance.application.queries import GetStudentAttendanceQuery
-from src.modules.student_management.application.queries import FindGroupHeadmanQuery
+from src.modules.student_management.domain.enums import Role
+from src.bot.common.resources.main_menu import main_menu
+
 from src.modules.common.infrastructure.config import DEBUG
 from src.modules.common.infrastructure.scheduling import AsyncJob
-from src.modules.edu_info.application.queries import FetchUniTimezonByGroupIdQuery, GetAllGroupsQuery
+from src.modules.edu_info.application.queries import GetAllGroupsQuery
 from src.modules.edu_info.domain import Group
 from src.modules.student_management.application.queries import (
     GetStudentsInfoFromGroupQuery,
@@ -21,13 +20,13 @@ from src.modules.student_management.application.queries import (
 from src.modules.student_management.domain import StudentInfo
 
 __all__ = [
-    "SendingJob",
+    "InformAboutUpdateJob",
 ]
 
 
 @final
-class SendingJob(AsyncJob):
-    """Send everyone message which allow user to choose lessons which will be visited."""
+class InformAboutUpdateJob(AsyncJob):
+    """Send everyone message with information about previous update."""
 
     _bot: Bot
     _build_container: Callable[[], AbstractAsyncContextManager[Injector]]
@@ -39,7 +38,7 @@ class SendingJob(AsyncJob):
         if not DEBUG:
             self._trigger = "cron"
             self._trigger_args = {
-                "hour": 7,
+                "hour": 20,
                 "minute": 00,
                 "day_of_week": "mon-sat",
             }
@@ -50,45 +49,30 @@ class SendingJob(AsyncJob):
             groups = await get_all_groups_query.execute()
             get_students_info_from_group_query = container.get(GetStudentsInfoFromGroupQuery)
 
-            fetch_group_timezone = container.get(FetchUniTimezonByGroupIdQuery)
-            find_group_headman_query = container.get(FindGroupHeadmanQuery)
-
             for group in groups:
-                timezone = await fetch_group_timezone.execute(group.id)
                 await self._send_to_group(
                     group,
                     get_students_info_from_group_query,
-                    find_group_headman_query,
-                    timezone,
                 )
 
     async def _send_to_group(
         self,
         group: Group,
         get_students_info_from_group_query: GetStudentsInfoFromGroupQuery,
-        find_group_headman_query: FindGroupHeadmanQuery,
-        timezone: str,
     ) -> None:
         students_info = await get_students_info_from_group_query.execute(group.id)
-        group_headman = await find_group_headman_query.execute(group.id)
 
         async with TaskGroup() as tg:
             for student_info in students_info:
-                tg.create_task(self._send_to_student(student_info, group_headman.id, timezone))
+                tg.create_task(self._send_to_student(student_info))
 
-    async def _send_to_student(self, student_info: StudentInfo, headman_telegram_id: int, timezone: str) -> None:
+    async def _send_to_student(self, student_info: StudentInfo) -> None:
         async with self._build_container() as container:
-            get_student_attedance_query = container.get(GetStudentAttendanceQuery)
-            attendances = await get_student_attedance_query.execute(student_info.id)
-
             try:
                 await self._bot.send_message(
                     student_info.telegram_id,
-                    POLL_TEMPLATE,
-                    reply_markup=update_attendance_buttons(student_info.is_checked_in_today, attendances, timezone),
+                    "...",
+                    reply_markup=main_menu(Role.STUDENT),
                 )
             except TelegramForbiddenError:
-                await self._bot.send_message(
-                    headman_telegram_id,
-                    student_was_not_polled_warning_template(student_info)
-                )
+                pass
