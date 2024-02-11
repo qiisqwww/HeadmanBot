9144d9dc121f83a46c6b90f02b1a5eddb9939cc8
NeoTheBestDeveloper <neothebestmain@protonmail.com>
2024-02-11 12:15:17 +0300

    fix error with no lessons

--- src/bot/show_group_attendance/callbacks/choose_lesson_to_show_attendance.py
+++ src/bot/show_group_attendance/callbacks/choose_lesson_to_show_attendance.py
@@ -37,7 +37,10 @@ async def attendance_send_callback(
         return
 
     schedule = await get_today_schedule_query.execute(student.group_id)
-    choosen_lesson = next(lesson for lesson in schedule if lesson.id == callback_data.lesson_id)
+    choosen_lesson = next((lesson for lesson in schedule if lesson.id == callback_data.lesson_id), None)
+
+    if choosen_lesson is None:
+        await callback.message.edit_text("Сегодня пар нет.")
     group_attendance = await get_visit_status_for_group_students_query.execute(student.group_id, choosen_lesson.id)
 
     new_message = attendance_for_headmen_template(choosen_lesson, group_attendance, timezone)
