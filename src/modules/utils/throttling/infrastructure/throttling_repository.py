5be2afff486f958c9907d97023393936b2a1311e
NeoTheBestDeveloper <neothebestmain@protonmail.com>
2024-02-13 06:39:30 +0300

    refactor: move THROTTLING_EXPIRE_TIME from env file to throttling repo

--- src/modules/utils/throttling/infrastructure/throttling_repository.py
+++ src/modules/utils/throttling/infrastructure/throttling_repository.py
@@ -1,6 +1,5 @@
-from typing import final
+from typing import Final, final
 
-from src.modules.common.infrastructure import THROTTLING_EXPIRE_TIME
 from src.modules.common.infrastructure.repositories import RedisRepositoryImpl
 from src.modules.utils.throttling.application.repositories import ThrottlingRepository
 
@@ -11,8 +10,10 @@ __all__ = [
 
 @final
 class ThrottlingRepositoryImpl(ThrottlingRepository, RedisRepositoryImpl):
+    _THROTTLING_EXPIRE_TIME: Final[int] = 60  # One minute.
+
     async def increase_user_throttling_rate(self, user_id: str) -> int:
         return await self._con.incr(f"throttling_{user_id}")
 
     async def set_expire_time(self, user_id: str) -> None:
-        await self._con.expire(f"throttling_{user_id}", THROTTLING_EXPIRE_TIME)
+        await self._con.expire(f"throttling_{user_id}", self._THROTTLING_EXPIRE_TIME)
