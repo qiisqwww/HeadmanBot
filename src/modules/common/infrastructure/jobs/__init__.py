70b2f46eab8640c0b86aaab607d0444529375112
qiisqwww <qiisqwww@gmail.com>
2024-03-04 19:56:04 +0300

    feat: added temporary poll_by_role job to poll new headmans and students

--- src/modules/common/infrastructure/jobs/__init__.py
+++ src/modules/common/infrastructure/jobs/__init__.py
@@ -1,7 +1,9 @@
 from .sending_job import SendingJob
 from .inform_about_update_job import InformAboutUpdateJob
+from .poll_by_role_job import PollByRoleJob
 
 __all__ = [
     "SendingJob",
-    "InformAboutUpdateJob"
+    "InformAboutUpdateJob",
+    PollByRoleJob
 ]
