4e2d9a3caabd5eb1b6f21db6bf237680a72d324f
NeoTheBestDeveloper <neothebestmain@protonmail.com>
2024-03-04 21:46:46 +0300

    pid

--- src/modules/common/infrastructure/config/logger_config.py
+++ src/modules/common/infrastructure/config/logger_config.py
@@ -10,6 +10,7 @@ __all__ = [
 def configurate_logger() -> None:
     logger.add(
         LOGGING_PATH,
+        level="INFO",
         compression="zip",
         rotation="500 MB",
         enqueue=True,
