310be1c79cc83076ecb0d876be9d74102b36a916
qiisqwww <qiisqwww@gmail.com>
2024-07-23 18:23:38 +0300

    added button with url to RTU MIREA map in show schedule menu

--- src/bot/show_schedule/command.py
+++ src/bot/show_schedule/command.py
@@ -3,7 +3,8 @@ from aiogram.types import Message
 from src.bot.common import CommandFilter, RootRouter, Router, TelegramCommand
 from src.bot.show_schedule.resources.templates import CHOOSE_SCHEDULE_PERIOD_TEMPLATE
 from src.bot.show_schedule.resources.inline_buttons import show_choose_period_buttons
-from src.modules.student_management.domain import Role
+from src.modules.student_management.domain import Role, Student
+from src.modules.edu_info.application.queries import FetchUniAliasByGroupIdQuery
 
 __all__ = [
     "include_show_schedule_command_router",
@@ -21,8 +22,13 @@ def include_show_schedule_command_router(root_router: RootRouter) -> None:
 
 
 @show_schedule_command_router.message(CommandFilter(TelegramCommand.SHOW_SCHEDULE))
-async def get_attendance_command(message: Message) -> None:
+async def get_attendance_command(
+        message: Message,
+        student: Student,
+        fetch_uni_alias_by_group_id_query: FetchUniAliasByGroupIdQuery
+) -> None:
+    uni_alias = await fetch_uni_alias_by_group_id_query.execute(student.group_id)
     await message.answer(
         CHOOSE_SCHEDULE_PERIOD_TEMPLATE,
-        reply_markup=show_choose_period_buttons()
+        reply_markup=show_choose_period_buttons(uni_alias)
     )
