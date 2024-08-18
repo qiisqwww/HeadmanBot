d829300b62de4105417a65519f1bcf4fc94e09cc
NeoTheBestDeveloper <neothebestmain@protonmail.com>
2024-08-18 22:30:46 +0300

    fixes

--- scripts/generate_ssl_certificates.sh
+++ scripts/generate_ssl_certificates.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 echo "Setup ssl ceritificates"
-openssl req -newkey rsa:4096 -sha256 -nodes -keyout nginx/headman_bot.key -x509 -out nginx/headman_bot.pem -subj "/C=US/ST=New York/L=Brooklyn/O=Example Brooklyn Company/CN=YOURDOMAIN.EXAMPLE"
+openssl req -newkey rsa:4096 -sha256 -nodes -keyout headman_bot.key -x509 -out headman_bot.pem -subj "/C=US/ST=New York/L=Brooklyn/O=Example Brooklyn Company/CN=YOURDOMAIN.EXAMPLE"
+cp headman_bot.pem headman_bot.key nginx/
